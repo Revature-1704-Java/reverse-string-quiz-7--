9b18fe4e4816110976f41710908303354aeb3507
Phil <regop412@gmail.com>
2017-12-20 10:09:10 -0500

    tests pass

--- src/main/java/quiz/ReverseString.java
+++ src/main/java/quiz/ReverseString.java
@@ -2,6 +2,11 @@ package quiz;
 
 public class ReverseString {
     public String reverse(String input) {
-        return new StringBuffer(input).reverse().toString();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            sb.append(input.charAt(input.length() - 1 - i));
+        }
+        return sb.toString();
+        // return new StringBuffer(input).reverse().toString();
     }
 }
